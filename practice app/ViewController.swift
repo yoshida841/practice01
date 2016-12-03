552739507fde645573b5bda6246581f88f864a72
Kazuhiro Yoshida <kazuhiro_yoshida@Kazuhiros-MacBook-Pro.local>
2016-12-03 20:44:54 +0900

    Initial Commit

---
+++ practice app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practice app
+//
+//  Created by Kazuhiro Yoshida on 2016/12/03.
+//  Copyright © 2016年 Kazuhiro Yoshida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
